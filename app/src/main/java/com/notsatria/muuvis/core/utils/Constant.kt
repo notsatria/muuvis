da48df1d7b50a47c21aeb7146fcbdeee6adaaeee
notsatria <buanasatriaa@gmail.com>
2024-09-20 08:24:03 +0700

    feat: add network call (now playing movies) and add shimmering effect

---
+++ app/src/main/java/com/notsatria/muuvis/core/utils/Constant.kt
@@ -0,0 +1,7 @@
+package com.notsatria.muuvis.core.utils
+
+object Constant {
+    const val BASE_URL = "https://api.themoviedb.org/3/movie"
+    const val API_KEY =
+        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYTFmMjQzNWNlYmRhNDcyYjYwMGFiNTExNGMzYTU0NSIsIm5iZiI6MTcyNjczNzQxNC4yOTg5NTIsInN1YiI6IjY2ZTk0YzExODJmZjg3M2Y3ZDFlYTJjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IfCMqSwvWOiuNaTOj1iMClWqSHUQIE8VgS3FkgZspcQ"
+}

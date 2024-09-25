1262e850b001f9373b3d6fe55d6845ab8951aefd
Satria <92832439+notsatria@users.noreply.github.com>
2024-09-25 19:44:52 +0700

    little update

--- core/src/main/java/com/notsatria/core/utils/Constant.kt
+++ core/src/main/java/com/notsatria/core/utils/Constant.kt
@@ -3,7 +3,7 @@ package com.notsatria.core.utils
 object Constant {
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val API_KEY =
-        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYTFmMjQzNWNlYmRhNDcyYjYwMGFiNTExNGMzYTU0NSIsIm5iZiI6MTcyNjczNzQxNC4yOTg5NTIsInN1YiI6IjY2ZTk0YzExODJmZjg3M2Y3ZDFlYTJjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IfCMqSwvWOiuNaTOj1iMClWqSHUQIE8VgS3FkgZspcQ"
+        "YOUR_API_KEY"
     const val POSTER_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original"
     const val BACKDROP_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w154"
 }

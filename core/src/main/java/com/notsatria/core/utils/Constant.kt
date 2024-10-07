2b7e67882ee596ae2c09e4cc6662017a69999487
notsatria <buanasatriaa@gmail.com>
2024-10-07 21:36:03 +0700

    update constant

---
+++ core/src/main/java/com/notsatria/core/utils/Constant.kt
@@ -0,0 +1,9 @@
+package com.notsatria.core.utils
+
+object Constant {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val POSTER_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+    const val BACKDROP_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w780"
+    const val API_KEY =
+        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYTFmMjQzNWNlYmRhNDcyYjYwMGFiNTExNGMzYTU0NSIsIm5iZiI6MTcyNzY1ODk3MS4yNjQwOTEsInN1YiI6IjY2ZTk0YzExODJmZjg3M2Y3ZDFlYTJjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nNaki-RM6zmNWOFfV5-J9GG6dfOjZRmsXm5s_A1dIDg"
+}
